297f4bf13a21380b95522260f01fb230ba7bb50a
Flo <flo.mayerhofer99@gmail.com>
2022-12-01 21:59:36 +0100

    changed server file and backend urls

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 /** Force ssl */
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https')
-    res.redirect(`https://${req.header('host')}${req.url}`)
-  else
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.header('x-forwarded-proto') !== 'https')
+//     res.redirect(`https://${req.header('host')}${req.url}`)
+//   else
+//   next();
+// });
 
 app.use(express.static(__dirname + '/dist/frontend'));
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -5,8 +5,8 @@ import { Product } from '../model/product';
 import { environment } from '../../environments/environment';
 
 
-// const PRODUCT_API = 'http://localhost:3000/api/users';
-const PRODUCT_API = 'https://ebayshpock-backend.onrender.com/api/users';
+const PRODUCT_API = 'http://localhost:3000/api/users';
+// const PRODUCT_API = 'https://ebayshpock-backend.onrender.com/api/users';
  
 
 @Injectable({
